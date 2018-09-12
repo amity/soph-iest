9c2e727d9201eee83841fee68bcc09ae8aadfc8c
soph-iest <natejneumann@gmail.com>
2018-09-11 22:44:57 -0400

    fix on images on jp version + navbar

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -9,20 +9,36 @@ const Resume = ({ jp }) => {
       </h4>
       <object
         className="pdf-object"
-        data="../static/pdf/nathan-neumann-resume-eng.pdf"
+        data={
+          jp
+            ? "../../static/pdf/nathan-neumann-resume-eng.pdf"
+            : "../static/pdf/nathan-neumann-resume-eng.pdf"
+        }
         type="application/pdf"
         width="100%"
         height="100%"
       >
         <iframe
-          src="../static/pdf/nathan-neumann-resume-eng.pdf"
+          src={
+            jp
+              ? "../../static/pdf/nathan-neumann-resume-eng.pdf"
+              : "../static/pdf/nathan-neumann-resume-eng.pdf"
+          }
           width="100%"
           height="100%"
           style={{ border: "none" }}
         >
           This browser does not support PDFs. Please download the PDF to view
           it:&nbsp;
-          <a href="../static/pdf/nathan-neumann-resume-eng.pdf">Download PDF</a>
+          <a
+            href={
+              jp
+                ? "../../static/pdf/nathan-neumann-resume-eng.pdf"
+                : "../static/pdf/nathan-neumann-resume-eng.pdf"
+            }
+          >
+            Download PDF
+          </a>
         </iframe>
       </object>
     </div>
